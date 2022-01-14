0d06759c7cbd44c47742bdc9fbc2872e96b73449
Abinesh <2001mrabi@gmail.com>
2022-01-14 19:27:45 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
+          Get started by Creating New branch oh GitHub{" "}
         </p>
       </main>
 
