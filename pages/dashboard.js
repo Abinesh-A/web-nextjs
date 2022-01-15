3c7c49f8211258880902b0b4f0c31809e90d3fa3
S Mohamed Farhan <mohamedfarhan1424@gmail.com>
2022-01-15 14:18:28 +0530

    second

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import {useRouter} from 'next/router'
 function dashboard() {
     const history=useRouter();
-   const signout=()=>{
-       history.push('/');
-   }
+   
     return (
         <div>
             <h1>HEllo Signed in successfully</h1>
-            <button onClick={signout}>Signout</button>
         </div>
     )
 }
