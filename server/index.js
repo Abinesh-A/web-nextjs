9d6638ce4ed6131a22bc3464cf798ac313cf88c7
Abinesh <abinesh@xcelcorp.com>
2022-09-02 16:11:17 +0530

    ./

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const port = 8080;
 const http = require("http").Server(app);
 const io = require("socket.io")(http,{
     cors:{
-        origin:["http://localhost:3000"]
+        origin:"*"
     }
 });
 
