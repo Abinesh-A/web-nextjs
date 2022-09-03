06714d4b8a20a751a88a05a84a0fc90d3755a61b
Abinesh <abinesh@xcelcorp.com>
2022-09-03 10:47:01 +0530

    ./

--- client/pages/xox.js
+++ client/pages/xox.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { io } from "socket.io-client";
 import Nav from "../Components/Nav";
 import JoinRoom from "../Components/JoinRoom";
