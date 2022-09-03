fa8fc36b918fcb7383a65b42ab49a4312e1b00a8
Abinesh <abinesh@xcelcorp.com>
2022-09-03 16:19:05 +0530

    ./

--- client/Components/GoogleloginComponent.js
+++ client/Components/GoogleloginComponent.js
@@ -5,22 +5,19 @@ import { useSelector } from "react-redux";
 import styles from "../styles/Loginpage.module.css";
 
 const GoogleloginComponent = (props) => {
-  const dontenv = require("../dotenv");
-  dontenv.config();
-
   const state = useSelector((state) => state);
   // useEffect(()=>{
   //   gapi.load("client:auth2",()=>{
   //     gapi.auth2.init({clientId:"326867438574-fjbedp8rse7jpf9i4kg16vikjug3v9u5.apps.googleusercontent.com"})
   //   })
   // })
-  console.log(state.isAuthenticate);
+  console.log(state.isAuthenticate)
   const logined = (response) => {
     console.log(response);
     props.signinResponse(true, response.profileObj);
   };
   const notlogined = (response) => {
-    console.log(response);
+    console.log(response)
     props.signinResponse(false, null);
   };
   return (
@@ -42,7 +39,7 @@ const GoogleloginComponent = (props) => {
         isSignedIn={state.isAuthenticate}
         onSuccess={logined}
         onFailure={notlogined}
-        cookiePolicy={"single_host_origin"}
+        cookiePolicy={'single_host_origin'}
         clientId={process.env.clientId}
         // clientId="510258406716-hcdps2qjfkabro5v287ck9477tor4jvb.apps.googleusercontent.com"
         // clientId="326867438574-fjbedp8rse7jpf9i4kg16vikjug3v9u5.apps.googleusercontent.com"
